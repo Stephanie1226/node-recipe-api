da6e5bd72fd9d603f423050842185aff12ed2b9c
Chieh Wang <chiehwang1226@gmail.com>
2020-10-16 23:13:33 -0600

    added cors middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var cors = require('cors')
 require('./db/mongoose')
 /// Our router setup
 const userRouter = require('./routers/user')
@@ -6,6 +7,7 @@ const recipeRouter = require('./routers/recipe')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(recipeRouter)

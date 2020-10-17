25c969ada8db4ba1bc6f3232a03fccf7d7419fbb
Chieh Wang <chiehwang1226@gmail.com>
2020-10-16 23:54:13 -0600

    added Client-Side Routing

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use(userRouter)
 app.use(recipeRouter)
 
